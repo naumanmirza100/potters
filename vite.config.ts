fc882bb78b211b128e9ba90005d2ed4d940ed364
Umer Younas <umer.younaskhan@gmail.com>
2026-03-19 20:45:28 +0500

    Add Vercel added

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   publicDir: "public",
-  base: "./",
+  base: "/",
   css: {
     postcss: {
       plugins: [tailwind()],
